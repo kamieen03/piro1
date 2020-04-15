e9874c11ad64c950fd839cfa66e0f59bbd643b73
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-04-15 23:09:56 +0200

    Final

--- prog.py
+++ prog.py
@@ -13,10 +13,6 @@ def rotate_to_common(img, h, w):
     left  = np.count_nonzero(img[:,:5])/h
     right = np.count_nonzero(img[:,-5:])/h
     minimal = min([upper, lower, left, right])
-    if DEBUG:
-        print(f'  {upper:.2} \
-                \n{left:.2}  {right:.2}\
-            \n    {lower:.2}')
     if minimal != upper:
         if minimal == lower:
             angle = -180
@@ -41,7 +37,6 @@ def rotate_to_common(img, h, w):
     return img
 
 def contour(img, name):
-    print("Processing ", name)
     # rotate so sides of rectangle are straight
     H,W = img.shape
     size = max(H,W)+10
@@ -69,7 +64,7 @@ def contour(img, name):
         img = img[10:,:]
     else:
         img = img[:-10,:]
-    img = img[:,3:-3]
+    img = img[:,5:-6]
     nz = cv2.findNonZero(img)
     x, y, w, h = cv2.boundingRect(nz)
     img = img[y:y+h, x:x+w]
@@ -101,31 +96,38 @@ def compare(chars, rev_chars):
     rev_norms = [np.linalg.norm(vec) for vec in rev_chars]
     for i in range(len(chars)):
         for j in range(i):
-            score1 = chars[i].dot(chars[j])/(norms[i]*norms[j])
-            score2 = rev_chars[i].dot(chars[j])/(rev_norms[i]*norms[j])
-            similarity_mat[i][j] = max(score1, score2)
+            score = rev_chars[i].dot(chars[j])/(rev_norms[i]*norms[j])
+            similarity_mat[i][j] = score
             similarity_mat[j][i] = similarity_mat[i][j]
     return similarity_mat
 
 def parse_similarity_mat(sim_mat):
+    for row in sim_mat:
+        r = list(enumerate(row))
+        r = sorted(r, key = lambda i_pr: i_pr[1])[::-1]
+        r = [p[0] for p in r]
+        for p in r:
+            print(p, end=' ')
+        print()
     return sim_mat.argmax(1)
 
 def compute_accuracy(result, path):
-    with open(path+'correct.txt') as f:
+    with open(os.path.join(path,'correct.txt')) as f:
         correct = np.array([int(num.strip()) for num in f.readlines()])
-    print(correct)
-    print(result)
+    #print(correct)
+    #print(result)
     acc = (correct == result).sum()/len(correct)*100
-    print(acc)
+    #print(acc)
     return acc
 
 def main():
-    path = f'daneA/set{sys.argv[1]}/'
+    path = os.path.abspath(sys.argv[1])
+    N = int(sys.argv[2])
     # get image names
     img_names = sorted([img for img in os.listdir(path) if img[-4:] == '.png'],
-            key = lambda s: int(s.split('.')[0]))
+            key = lambda s: int(s.split('.')[0]))[:N]
     # read images into memory
-    imgs = [cv2.imread(path+img, 0) for img in img_names]
+    imgs = [cv2.imread(os.path.join(path,img), 0) for img in img_names]
     # get contours of images
     imgs = [contour(img, name) for (img, name) in zip(imgs, img_names)]
     # resize images to common size
@@ -138,7 +140,7 @@ def main():
     rev_chars = [characteristic(rot_180(img)) for img in imgs]
     similarity_mat = compare(chars, rev_chars)
     result = parse_similarity_mat(similarity_mat)
-    compute_accuracy(result, path)
+    #compute_accuracy(result, path)
 
 if __name__ == '__main__':
     main()
