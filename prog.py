b4f0a3f1fef90eb56be43cf1d14900d59af0e6c2
filip <filip.bonczyk@student.put.poznan.pl>
2020-04-15 12:24:58 +0200

    side detection

--- prog.py
+++ prog.py
@@ -4,8 +4,52 @@ import cv2
 import numpy as np
 import sys
 import os
+import math
 
 DEBUG = False
+
+def get_border_points(edges):
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 15, maxLineGap=30)
+    lines = lines[:, 0]
+
+    # Find base line - it is the logest detected line 
+    base_line_idx = np.argmax([calc_length(line) for line in lines])
+    base_line = lines[base_line_idx]
+
+    # Find sides - check angle between lines and distance between endpoints
+    a, b = np.split(base_line, 2)
+    side1 = get_side_line(a, base_line, lines)
+    side2 = get_side_line(b, base_line, lines)
+
+    return np.array([base_line, side1,side2])
+
+
+def get_side_line(e, base_line, lines):
+    angle_acc = 5
+    side_lines = [line for line in lines \
+        if (90 - angle_acc) < (abs(calc_angle(line) - calc_angle(base_line))) < (90 + angle_acc) and is_in_range(e, line)]
+
+    return side_lines[np.argmax([calc_length(line) for line in side_lines])]
+
+
+def is_in_range(e, line):
+    r = 60
+    square_range = math.pow(r, 2)
+    return math.pow(line[0]-e[0], 2) + math.pow(line[1]-e[1], 2) <= square_range \
+        or math.pow(line[2]-e[0], 2) + math.pow(line[3]-e[1], 2) <= square_range
+
+
+def calc_angle(l):
+    angle = math.atan2(l[1] - l[3], l[0] - l[2])
+    if angle < 0:
+        angle = angle + 2*np.pi
+    return angle * 180/np.pi
+
+
+def calc_length(line):
+    x1, y1, x2, y2 = line
+    return math.sqrt(((x1-x2)**2)+((y1-y2)**2))
+
 
 def rotate_to_common(img, h, w):
     upper = np.count_nonzero(img[:5,:])/w
@@ -43,6 +87,10 @@ def rotate_to_common(img, h, w):
 def contour(img, name):
     print("Processing ", name)
     # rotate so sides of rectangle are straight
+    preview = np.zeros(img.shape)
+    edges = cv2.Canny(img,50,150,apertureSize = 3)
+    l = get_border_points(edges)
+
     H,W = img.shape
     size = max(H,W)+10
     new_img = np.zeros((size,size), dtype=np.uint8)
@@ -52,6 +100,22 @@ def contour(img, name):
     _rect = cv2.minAreaRect(cnt[0])
     angle = _rect[2]
     _box = cv2.boxPoints(_rect)
+
+    #-----
+    # x1, y1, x2, y2 = base_line[0]
+    # cv2.line(preview, (x1, y1), (x2, y2), 255, 5)
+
+
+
+
+
+    # print(lens)
+
+    for x1, y1, x2, y2 in l:
+            cv2.line(preview, (x1, y1), (x2, y2), 255, 5)
+            # print(calc_angle(line[0]))
+    show(preview)
+
     center = sum(_box)/4
     M = cv2.getRotationMatrix2D(tuple(center),90+angle,1)
     size = max(img.shape)
@@ -69,11 +133,12 @@ def contour(img, name):
         img = img[10:,:]
     else:
         img = img[:-10,:]
-    img = img[:,3:-3]
+    img = img[:,5:-5]
     nz = cv2.findNonZero(img)
     x, y, w, h = cv2.boundingRect(nz)
     img = img[y:y+h, x:x+w]
     return img
+
 
 def rot_180(img):
     h, w = img.shape
@@ -81,19 +146,24 @@ def rot_180(img):
     img = cv2.warpAffine(img, M, img.shape[::-1])
     return img
 
+
 def show(img):
     cv2.imshow("XD", img)
     while True:
         if cv2.waitKey(0) == 27:
             break
 
+
 def characteristic(contour):
     # get indices of first nonzero pixel in each column
+    # show(contour)
     return contour.argmax(0)
+
 
 def binarize(img):
     img[img!=0] = 255
     return img
+
 
 def compare(chars, rev_chars):
     similarity_mat = np.zeros((len(chars), len(chars)))
@@ -107,8 +177,10 @@ def compare(chars, rev_chars):
             similarity_mat[j][i] = similarity_mat[i][j]
     return similarity_mat
 
+
 def parse_similarity_mat(sim_mat):
     return sim_mat.argmax(1)
+
 
 def compute_accuracy(result, path):
     with open(path+'correct.txt') as f:
@@ -118,6 +190,7 @@ def compute_accuracy(result, path):
     acc = (correct == result).sum()/len(correct)*100
     print(acc)
     return acc
+
 
 def main():
     path = f'daneA/set{sys.argv[1]}/'
